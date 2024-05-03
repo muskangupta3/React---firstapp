686a797f6b8a791389a2cf6b9d90cc276af8f40b
Muskan Gupta <muskan_gupt@yahoo.com>
2024-05-03 07:26:17 -0700

    Week 3 - 3rd May

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav';
-import Promo from './components/Promo';
-import Intro1 from './components/Intro1';
-import Intro2 from './components/Intro2';
-import Intro3 from './components/Intro3';
-import Footer from './components/Footer';
+import Card from './components/Week 3/Card';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
+//Week 2
+    // <div>
+    //   <Nav/>
+    //   <Promo title="Bumper offer"/>
+    //   <Promo title="Second Mega Bumper offer"/>
+    //   <Intro1/>
+    //   <Intro2/>
+    //   <Intro3/>
+    //   <Footer/>
     // </div>
-    <div>
-      <Nav/>
-      <Promo title="Bumper offer"/>
-      <Promo title="Second Mega Bumper offer"/>
-      <Intro1/>
-      <Intro2/>
-      <Intro3/>
-      <Footer/>
+
+    //Week 3
+    <div className="App">
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3"/>
+      <Card h2="Second card's h2" h3="Second card's h3"/>
+      <Card h2="Third card's h2" h3="Third card's h3"/>
     </div>
   );
 }
