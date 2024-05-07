faf8500c5c921582152a9812395e5471f9e0a09d
Muskan Gupta <muskan_gupt@yahoo.com>
2024-05-06 18:15:37 -0700

    Week 2 Module 1- 6th May

---
+++ src/components/Week 1/Module1App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css';
+import Card from './Module 3/Card';
+
+function Module1App() {
+  return (
+//Week 2
+    // <div>
+    //   <Nav/>
+    //   <Promo title="Bumper offer"/>
+    //   <Promo title="Second Mega Bumper offer"/>
+    //   <Intro1/>
+    //   <Intro2/>
+    //   <Intro3/>
+    //   <Footer/>
+    // </div>
+
+    //Week 3
+    <div className="App">
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3"/>
+      <Card h2="Second card's h2" h3="Second card's h3"/>
+      <Card h2="Third card's h2" h3="Third card's h3"/>
+    </div>
+  );
+}
+
+export default Module1App;
