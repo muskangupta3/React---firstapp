230c5646da464f8cb151fed0e623789b7f853c8c
Muskan Gupta <muskan_gupt@yahoo.com>
2024-05-10 12:56:57 -0700

    Week 2 Module 2 - 10th May

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css'
+import React from "react";
+import Fruits from "./components/Week 2/Module 2/Fruits";
+import FruitsCounter from "./components/Week 2/Module 2/FruitsCounter";
 
 function App() {
+  const [fruits] = React.useState([
+    {fruitName: 'apple', id: 1},
+    {fruitName: 'apple', id: 2},
+    {fruitName: 'plum', id: 3},
+    {fruitName: 'apple', id: 4},
+    {fruitName: 'plum', id: 5},
+]);
+
   return (
-    <div>
-      <h1>Task: Add a button and handle a click event</h1>
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits fruits = {fruits}/>
+      <FruitsCounter fruits = {fruits}/>
     </div>
   );
 }
