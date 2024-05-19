ca93e45667086d0adb9cce806e4bc028804b2adc
Muskan Gupta <muskan_gupt@yahoo.com>
2024-05-18 20:01:57 -0700

    Week 3 Module 2 -  Audio , Video addition

--- src/components/Week 3/Module 2/Week3Module2.js
+++ src/components/Week 3/Module 2/Week3Module2.js
@@ -1,11 +1,42 @@
 import "./App.css";
 import logo from './components/Week 3/Module 2/Assets/logo.png'
+import ReactPlayer from "react-player/youtube";
 
 function Week3Module2() {
+
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  };
+
+  function toggle2()
+  {
+    if (bird2.paused) {
+      bird2.play();
+    } else {
+      bird2.pause();
+    }
+  }
+
   return (
     <div className="App">
       <h1>Task: Add an image below</h1>
       <img src={logo} alt="Logo" height={200}/>
+      <ReactPlayer playing={false} volume={0.5} url='https://www.youtube.com/watch?v=saQBW9Xs5eI'/>
+ 
+       <button onClick={toggle1}>Caspian Tern 1</button>
+      <button onClick={toggle2}>Caspian Tern 2</button>
     </div>
     
   );
